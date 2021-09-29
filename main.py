db73f5aa7e2505c2b0dcb898e43de436582ffca9
Ráfagan Abreu <rafagansa@outlook.com>
2021-09-28 21:27:05 -0300

    Objetivos e dependencia de log

---
+++ main.py
@@ -0,0 +1,12 @@
+# Objetivo: Mostrar informações sobre visitações do ecommerce
+
+# a) Acesso por mês (mm/yyyy)
+# Histórico de visitas agrupadas por dia
+# Total de visitas no mês
+# Total de novos usuários
+# Tempo médio de engajamento
+# Receita total
+
+# b) Acesso por produto
+# Filtro por tipo de atividade
+# Histórico dos 7 produtos mais acessados
