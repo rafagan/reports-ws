db313eb4bd10fb6b40b4ed697bd8c9def8f2a31e
Ráfagan Abreu <rafagansa@outlook.com>
2021-09-28 22:11:32 -0300

    Tratando apache logs

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-# Objetivo: Mostrar informações sobre visitações do ecommerce
+######
+
+# 1. Objetivo: Mostrar informações sobre visitações do ecommerce
 
 # a) Acesso por mês (mm/yyyy)
 # Histórico de visitas agrupadas por dia
@@ -10,3 +12,66 @@
 # b) Acesso por produto
 # Filtro por tipo de atividade
 # Histórico dos 7 produtos mais acessados
+
+######
+
+# 2. Estudando os dados
+# https://github.com/jwodder/apachelogs
+# https://apachelogs.readthedocs.io/en/stable/directives.html
+# http://httpd.apache.org/docs/current/mod/mod_log_config.html
+
+# vendas.ecotrilhasserracatarinense.com:443 189.6.235.31 - - [15/Sep/2021:00:00:33 -0300] "GET /carrinho/9885 HTTP/2.0" 200 5665 "https://vendas.ecotrilhasserracatarinense.com/carrinho/9885" "Mozilla/5.0 (iPhone; CPU iPhone OS 14_7_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Mobile/15E148 Safari/604.1"
+# vendas.ecotrilhasserracatarinense.com:443 189.6.235.31 - - [15/Sep/2021:00:00:33 -0300] "GET /theme/default/assets/css/styles.css?8 HTTP/2.0" 304 56 "https://vendas.ecotrilhasserracatarinense.com/carrinho/9885" "Mozilla/5.0 (iPhone; CPU iPhone OS 14_7_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Mobile/15E148 Safari/604.1"
+# 54.189.230.128 - - [15/Sep/2021:00:05:12 -0300] "GET / HTTP/1.1" 301 512 "-" "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:82.0) Gecko/20100101 Firefox/82.0"
+
+# remote_host: %h:
+# vendas.ecotrilhasserracatarinense.com:443
+# vendas.ecotrilhasserracatarinense.com:443
+#
+
+# remote_logname: %l
+# 189.6.235.31
+# 189.6.235.31
+# 54.189.230.128
+
+# remote_user: %u
+# - -
+# - -
+# - -
+
+# request_time: %t
+# [15/Sep/2021:00:00:33 -0300]
+# [15/Sep/2021:00:00:33 -0300]
+# [15/Sep/2021:00:05:12 -0300]
+
+# request_line: "%r"
+# GET /carrinho/9885 HTTP/2.0
+# GET /theme/default/assets/css/styles.css?8 HTTP/2.0
+# GET / HTTP/1.1
+
+# final_status: %>s
+
+# bytes_sent: %b
+
+# headers_in:
+# Referer: "%{Referer}i"
+# User-Agent: "%{User-Agent}i"
+
+def parse_apache_log(path):
+    entries = []
+
+    from apachelogs import LogParser
+    with open(path) as file:
+        parser = LogParser("%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"")
+        for entry in parser.parse_lines(file):
+            entries.append(entry)
+
+    return entries
+
+
+def run():
+    entries = parse_apache_log('logs/2021-09-15.log')
+    print(entries)
+
+
+run()
