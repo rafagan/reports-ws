61c439ae80460fb935d8a9afa0848e444d627c67
Ráfagan Abreu <rafagansa@outlook.com>
2021-10-11 21:16:05 -0300

    fix api

--- server/api.py
+++ server/api.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request
 
 from server.db import query_daily_visits, query_total_visits, query_total_new_visits, \
@@ -6,32 +8,37 @@ from server.db import query_daily_visits, query_total_visits, query_total_new_vi
 app = Flask(__name__)
 
 
-@app.route('/visits/{month}', methods=['GET'])
+@app.route('/hello', methods=['GET'])
+def hello():
+    return {'status': 'ok'}
+
+
+@app.route('/visits/<month>', methods=['GET'])
 def get_daily_visits(month):
     return {'status': 'ok', 'payload': query_daily_visits(month)}
 
 
-@app.route('/visits/{month}/total', methods=['GET'])
+@app.route('/visits/<month>/total', methods=['GET'])
 def get_total_visits(month):
     return {'status': 'ok', 'payload': query_total_visits(month)}
 
 
-@app.route('/visits/{month}/total/new', methods=['GET'])
+@app.route('/visits/<month>/total/new', methods=['GET'])
 def get_total_new_visits(month):
     return {'status': 'ok', 'payload': query_total_new_visits(month)}
 
 
-@app.route('/visits/{month}/average/engagement', methods=['GET'])
+@app.route('/visits/<month>/average/engagement', methods=['GET'])
 def get_avg_engagement_time_secs(month):
     return {'status': 'ok', 'payload': query_avg_engagement_time_secs(month)}
 
 
-@app.route('/sells/{month}/total', methods=['GET'])
+@app.route('/sells/<month>/total', methods=['GET'])
 def get_total_receipt(month):
     return {'status': 'ok', 'payload': query_total_receipt(month)}
 
 
-@app.route('/products/{activity_type}', methods=['GET'])
+@app.route('/products/<activity_type>', methods=['GET'])
 def get_most_visited_products(activity_type):
     amount = request.args.get('amount', 7)
     return {
@@ -41,5 +48,6 @@ def get_most_visited_products(activity_type):
 
 
 if __name__ == "__main__":
+    os.environ['FLASK_ENV'] = 'development'
     app.run(debug=True, host='0.0.0.0', port=5000)
 
